f6a0b24f9ec712956b9bf9d1bf5c99aef1cea0b5
Swati Jain <jain.swati71@gmail.com>
2023-09-09 19:18:46 +0200

    Added show details page

--- js/script.js
+++ js/script.js
@@ -5,8 +5,6 @@ const global = {
 async function displayPopularMovies(){
     const { results } = await fetchAPIData('movie/popular');
 
-    // console.log(results);
-
     results.forEach(movie => {
         const div = document.createElement('div');
         div.classList.add('card');
@@ -42,13 +40,11 @@ async function displayPopularMovies(){
 async function displayPopularshows(){
     const { results } = await fetchAPIData('tv/popular');
 
-    // console.log(results);
-
     results.forEach(show => {
         const div = document.createElement('div');
         div.classList.add('card');
         div.innerHTML = `
-        <a href="movie-details.html?id=${show.id}">
+        <a href="tv-details.html?id=${show.id}">
           ${
             show.poster_path
             ? `<img
@@ -109,7 +105,7 @@ async function displayMovieDetails(){
             <h2>${movie.title}</h2>
             <p>
               <i class="fas fa-star text-primary"></i>
-              ${movie.vote_average.toFixed(1)} / 10
+             ${movie.vote_average.toFixed(1)} / 10
             </p>
             <p class="text-muted">Release Date: ${movie.release_date}</p>
             <p>
@@ -157,47 +153,46 @@ async function displayShowDetails(){
   <div class="details-top">
         <div>
         ${
-          movie.poster_path
+          show.poster_path
           ? `<img
-          src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+          src="https://image.tmdb.org/t/p/w500${show.poster_path}"
           class="card-img-top"
-          alt="${movie.title}"
+          alt="${show.name}"
         />`
           : `<img
           src="../images/no-image.jpg"
           class="card-img-top"
-          alt="${movie.title}"
+          alt="${show.name}"
         />`
         }
         </div>
         <div>
-          <h2>${movie.title}</h2>
+          <h2>${show.name}</h2>
           <p>
             <i class="fas fa-star text-primary"></i>
-            ${movie.vote_average.toFixed(1)} / 10
+            ${show.vote_average.toFixed(1)} / 10
           </p>
-          <p class="text-muted">Release Date: ${movie.release_date}</p>
+          <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
           <p>
-          ${movie.overview}
+          ${show.overview}
           </p>
           <h5>Genres</h5>
           <ul class="list-group">
-            ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
           </ul>
-          <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
         </div>
       </div>
       <div class="details-bottom">
-        <h2>Movie Info</h2>
+        <h2>Show Info</h2>
         <ul>
-          <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
-          <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
-          <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
-          <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          <li><span class="text-secondary">Number of Episodes:</span> ${(show.number_of_episodes)}</li>
+          <li><span class="text-secondary">Last Episode To Air:</span> ${(show.last_episode_to_air.name)}</li>
+          <li><span class="text-secondary">Status:</span> ${show.status}</li>
         </ul>
         <h4>Production Companies</h4>
         <div class="list-group">
-        ${movie.production_companies.map((company) => `<span>${company.name}</span>`).join('')}
+        ${show.production_companies.map((company) => `<span>${company.name}</span>`).join('')}
         </div>
       </div>
   `;
@@ -283,6 +278,7 @@ function init(){
             console.log('Movie Details');
             break;  
         case '/tv-details.html':
+          displayShowDetails();
             console.log('TV Details');
             break;
         case '/search.html':
